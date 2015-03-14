227401aef963e4f65dd103dadeee663f94d12aa1
Jordan <j6k4m8@gmail.com>
2015-03-14 17:03:05 -0400

    Add json decoding

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import settings
 import urllib
 import sys
 import re
+import json
 
+#=============================================================================={ utils
+def validate_task(task):
+    # A string must have matched backticks if it has any
+    return len(re.findall('`.+`', task)) <= 1
+
+
+def json_to_list(task_list):
+    return json.loads(task_list)
+
+#=============================================================================={ requests
 def get_all_tasks():
     params = urllib.urlencode({
         "key": "1",
     })
     request_url = "http://" + settings.root_url + "/api/tasks/get"
     res = urllib.urlopen(request_url, params)
-    print(res.read())
-
-
-def validate_task(task):
-    # A string must have matched backticks if it has any
-    return len(re.findall('`.+`', task)) <= 1
+    print(json_to_list(res.read()))
 
 
 def send_new_task(task):
@@ -34,6 +40,9 @@ def send_new_task(task):
         request_url = "http://" + settings.root_url + "/api/tasks/new"
         res = urllib.urlopen(request_url, params)
         print(res.read())
+
+
+
 
 
 if len(sys.argv) == 1:
